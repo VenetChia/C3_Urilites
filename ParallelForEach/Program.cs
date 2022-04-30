f826c857a03b0870432333849d33819a7d08ddba
venetchia <venetchia@mail.ru>
2022-04-30 18:22:45 +0300

    Async

---
+++ ParallelForEach/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelForEach
+{
+    class Program
+    {
+        static int[] data;
+        static void Main()
+        {
+            Console.WriteLine("Main thread is starting");
+            data = new int[1000000000];
+            for (int i = 0; i < data.Length; i++) data[i] = i;
+            data[100] = -10;
+            ParallelLoopResult loopResult = Parallel.ForEach(data, (value, plstate) =>
+            {
+                Console.WriteLine($"Value: {value}");
+                if (value < 0) plstate.Break();
+            });
+            if (!loopResult.IsCompleted)
+                Console.Write("\nLoop Terminated early because a negative value was encountered\n in iteration number " + loopResult.LowestBreakIteration + ".\n");
+
+            Console.WriteLine("Main thread is ending");
+        }
+        static void DisplayData(int v, ParallelLoopState pls)
+        {
+            if (v < 0) pls.Break();
+            Console.WriteLine("Value: " + v);
+        }
+    }
+}
--- paralelInvoke/Program.cs
+++ paralelInvoke/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace paralelInvoke
 {
@@ -6,7 +8,52 @@ namespace paralelInvoke
     {
         static void Main()
         {
-            Console.WriteLine();
+            Console.WriteLine("Main thread is starting");
+            Parallel.Invoke(() =>
+           {
+               Console.WriteLine("Expression 1 is starting");
+               for (int i = 0; i < 5; i++)
+               {
+                   Thread.Sleep(500);
+                   Console.WriteLine("Expression 1 count is " + i);
+               }
+               Console.WriteLine("Expression 1 is terminating");
+           }, (Action)delegate ()
+           {
+               Console.WriteLine("Expression 2 is starting");
+               for (int i = 0; i < 10; i++)
+               {
+                   Thread.Sleep(500);
+                   Console.WriteLine("Expression 2 count is " + i);
+               }
+               Console.WriteLine("Expression 2 is terminating");
+           });
+
+            //пример2
+            ParallelOptions parallelOptions = new ParallelOptions();
+            Parallel.Invoke(Method1, Method2);
+            Console.WriteLine("Main thread is ending");
+
+        }
+        static void Method1()
+        {
+            Console.WriteLine("Expression 1 is starting");
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Expression 1 count is " + i);
+            }
+            Console.WriteLine("Expression 1 is terminating");
+        }
+        static void Method2()
+        {
+            Console.WriteLine("Expression 2 is starting");
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Expression 2 count is " + i);
+            }
+            Console.WriteLine("Expression 2 is terminating");
         }
     }
 }
